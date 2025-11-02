532f7f06c07d931031bece90480dd25aa8f93b15
harameter <haram1000068@gmail.com>
2025-11-03 02:57:04 +0900

    feat: 당첨 번호 및 보너스 번호 포함 개수/여부 확인 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,42 +1,86 @@
 /*
- * Lotto.java   0.1 2025/10/29
+ * Lotto.java   0.2 2025/11/1
  */
 package lotto.domain;
 
 import java.util.*;
 
 /**
- * 로또 번호에 대한 검증 등 각 로또 단위에 대한 기능 처리를 담당.
+ * 구매한 각 로또 객체 클래스
  *
- * @version 0.2 2025/11/1
+ * @version 0.3 2025/11/3
  * @author haram
  */
 public class Lotto {
-    private final List<Integer> numbers;
+	private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
-    }
+	public Lotto(List<Integer> numbers) {
+		validate(numbers);
+		this.numbers = numbers;
+	}
 
-    /**
-     * 입력된 로또 번호 내의 각 숫자가 올바른 값인지 검증.
-     * 입력값이 공백이거나 숫자가 아닌 경우 등은 파싱 단계에서 진행하므로 여기서 중복 처리할 필요 없음.
-     *
-     * @param numbers 파싱된 로또번호 입력값울 담은 리스트
-     */
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-        Set<Integer> duplicateCheck = new HashSet<>(numbers);
-        if (duplicateCheck.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
-        }
-        for (Integer n : numbers) {
-            if ((n < 1) || (n > 45)) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자만 선택 가능합니다.");
-            }
-        }
-    }
+	/**
+	 * 입력된 당첨 번호 내의 각 숫자가 올바른 값인지 검증.
+	 * 입력값이 공백이거나 숫자가 아닌 경우 등은 파싱 단계에서 진행하므로 여기서 중복으로 처리할 필요 없음.
+	 *
+	 * @param numbers 파싱된 로또번호 입력값울 담은 리스트
+	 */
+	private void validate(List<Integer> numbers) {
+		if (numbers.size() != 6) {
+			throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+		}
+		Set<Integer> duplicateCheck = new HashSet<>(numbers);
+		if (duplicateCheck.size() != 6) {
+			throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+		}
+		for (Integer n : numbers) {
+			if ((n < 1) || (n > 45)) {
+				throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자만 선택 가능합니다.");
+			}
+		}
+	}
+
+	/**
+	 * 로또 번호 출력할 때 정렬된 번호 리스트가 필요하여 작성
+	 *
+	 * @return 정렬된 로또 번호 리스트
+	 */
+	public List<Integer> getSortedNumbers() {
+		List<Integer> copy = new ArrayList<>(numbers);
+		Collections.sort(copy);
+		return copy;
+	}
+
+	/**
+	 * 당첨 번호와 몇 개가 일치하는지 계산하는 메서드
+	 *
+	 * @param lottoWinning 당첨번호가 담겨있는 LottoWinning 클래스 객체
+	 * @return 당첨 번호 일치 개수
+	 */
+	public int countMatch(LottoWinning lottoWinning) {
+		int cnt = 0;
+		for (Integer n : numbers) {
+			if (lottoWinning.contains(n)) {
+				cnt++;
+			}
+		}
+		return cnt;
+	}
+
+	/**
+	 * 보너스 번호를 포함하는지 확인하는 메서드
+	 *
+	 * @param lottoWinning 보너스 번호가 담겨있는 LottoWinning 클래스 객체
+	 * @return 보너스 번호 포함 여부
+	 */
+	public boolean bonusMatch(LottoWinning lottoWinning) {
+		for (Integer n : numbers) {
+			if(lottoWinning.containBonus(n)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+
 }
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,30 +1,49 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LottoTest {
-    @Test
-    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+	@Test
+	void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
+		assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+				.isInstanceOf(IllegalArgumentException.class);
+	}
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
-    @Test
-    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+	@DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+	@Test
+	void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
+		assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+				.isInstanceOf(IllegalArgumentException.class);
+	}
 
-    @DisplayName("로또 번호에 범위가 맞지 않는 숫자가 있으면 예외가 발생한다.")
-    @Test
-    void 로또_번호에_범위에_맞지_않는_숫자가_있으면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+	@DisplayName("로또 번호에 범위가 맞지 않는 숫자가 있으면 예외가 발생한다.")
+	@Test
+	void 로또_번호에_범위에_맞지_않는_숫자가_있으면_예외가_발생한다() {
+		assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+				.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("당첨 번호 개수 일치 확인")
+	@Test
+	void 당첨_번호_개수_일치_확인() {
+		Lotto l = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+		LottoWinning testLotto = new LottoWinning(List.of(2, 3, 4, 5, 6, 7), 1);
+		Assertions.assertEquals(5, l.countMatch(testLotto));
+	}
+
+	@DisplayName("보너스 번호 일치 확인")
+	@Test
+	void 보너스_번호_일치_확인() {
+		Lotto l = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+		LottoWinning testLotto = new LottoWinning(List.of(2, 3, 4, 5, 6, 7), 1);
+		Assertions.assertTrue(l.bonusMatch(testLotto));
+	}
 }
